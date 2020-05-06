c22a9146eca9b6c8bfa485c5339e8e2e7b3b208e
Vladimir TROIS <vladimir.trois@nexworld.fr>
2020-05-06 08:37:53 +0200

    streams filtering

---
+++ src/main/java/config/MyConfig.java
@@ -0,0 +1,23 @@
+package config;
+
+public class MyConfig
+{
+    //Kafka Configuration
+    public static final String KAFKA_HOST_ADRESS = "35.205.126.252";
+    public static final String STREAMS_DIR_CONFIG = "/tmp/kafka/kafka-streams";
+
+    private static final String KAFKA_HOST_PORT = "9093";
+    private static final String KAFKA_SCHEMA_URL_PORT = "8081";
+
+    public static String getKafkaSchemaUrl()
+    {
+        return "http://" + KAFKA_HOST_ADRESS + ":" + KAFKA_SCHEMA_URL_PORT;
+    }
+
+    public static String getKafkaServerUrl()
+    {
+        return KAFKA_HOST_ADRESS + ":" + KAFKA_HOST_PORT;
+    }
+
+
+}
